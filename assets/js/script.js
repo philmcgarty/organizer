62a77920ad23d61aa61111e6416d48a68fbf73a0
Phil McGarty <philmcgarty@hotmail.com>
2022-03-20 11:20:21 -0400

    re-load tasks on page refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var taskArray = [];
+var allTasksArray = [];
 
 var currentDate = moment().format('dddd MMMM Do YYYY');
 
@@ -6,6 +6,33 @@ var taskArea = $(".tasks");
 
 $(currentDay).text(currentDate);
 
+
+// LOAD TASKS
+var loadTasks = function(){
+    allTasksArray = JSON.parse(localStorage.getItem("taskList"));
+
+    if (!allTasksArray){
+        allTasksArray = [];
+    };
+
+    for (i=0;i<allTasksArray.length;i++){
+        var hour = allTasksArray[i];
+        var taskTime = hour[0];
+        var taskText = hour[1];
+        // locate correct row
+        var findHour = document.getElementById(taskTime)
+        // locates decendent span
+        var findSpan = $(findHour).children(".tasks").children("span");
+        // assigns task text to correct span element
+        $(findSpan).text(taskText);
+    }   
+};
+
+
+// SAVE TASK ARRAY TO LOCAL STORAGE
+var saveTasks= function(){
+    localStorage.setItem("taskList", JSON.stringify(allTasksArray));
+};
 
 
 // CHANGE TO TEXT INPUT AND BACK - used taskmaster pro for reference
@@ -32,9 +59,23 @@ $(taskArea).on("click", function() {
         var span = $("<span>").text(newText).attr("class", "task-item");      
         // replaces input with span
         $(textInput).replaceWith(span);
+
+        // set variables to save in array
+        var taskTime = $(this).parent().attr("id");
+        // console.log(taskTime);
+        var newTask = [taskTime, newText]
+             
+        
+        //console.log(newTaskArray);
+        allTasksArray.push(newTask);
+        allTasksArray.sort((a,b) => {return a.time - b.time;});
+        console.log(allTasksArray);
+
+        saveTasks();
+
     })
     
 
 });
 
-
+loadTasks();
